63f7f5278da7aef88408d6c895f8882eeac834c1
lucas8 <luc.linux@mailoo.org>
2013-03-04 15:57:50 +0100

    Fin de l'interface des inotify tools.

--- src/inotools.h
+++ src/inotools.h
@@ -4,6 +4,12 @@
 
 int ino_init();
 int ino_close();
+char* ino_error(); // Retourne une str indiquant décrivant l'erreur survenue
+
+typedef int InoWatch;
+#define INOERR -1
+InoWatch ino_watch(const char* path); // Retourne INOERR en cas de problème, utiliser ino_error pour avoir une description
+void ino_unwatch(InoWatch wd);
 
 enum InoAction{ATTRIB, // Modifications permissions, timestamp ...
 	WRITE, // Fichier modifié (fermeture en écriture)
@@ -19,6 +25,9 @@ struct InoEvent
 	int dir;
 	char* path;
 };
+
+void ino_pollEvent(); // Met à jour les évènements stockés
+int ino_getEvent(InoEvent* ev, InoWatch wd); // Retourne le nombre d'évènement restant
 
 #endif
 
