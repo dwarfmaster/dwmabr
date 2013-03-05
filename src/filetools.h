607cec1169a5c3b57f6b2672701f902dd3eae812
lucas8 <luc.linux@mailoo.org>
2013-03-05 09:14:28 +0100

    Début de l'interface des file tools.

--- src/filetools.h
+++ src/filetools.h
@@ -2,5 +2,18 @@
 #ifndef DEF_FILETOOLS
 #define DEF_FILETOOLS
 
+#include <dirent.h>
+
+char** parts(const char* path); // Découpe un chemin : /home/xxx/Documents/img.png -> home, xxx, Documents, img.png
+char* filename(const char* path); // /home/xxx/Documents/img.png -> img.png, renvoie NULL si path pointe sur un dossier
+char* dir(const char* path); // /home/xxx/Documents/img.png -> /home/xxx/Documents/
+
+int readInt(FILE* f);
+unsigned int readUInt(FILE* f);
+double readDouble(FILE* f);
+char* readWord(FILE* f);
+
+struct dirent** dirContain(DIR* dir, int* length); // Écrit la taille lue dans length, le retour doit être libéré
+
 #endif
 
